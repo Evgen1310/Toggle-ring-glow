724f08f04a5d3dc6b1ada20475f01a050d0cf32e
Chungus <no.spam4maz@gmail.com>
2026-05-24 20:53:11 +0100

    Fix null lightSourceId memory leak and toggle-on failure

--- CodePatches.cs
+++ CodePatches.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewValley.Objects;
+using System.Collections.Generic;
 
 namespace ToggleRingGlow
 {
     public partial class ModEntry
     {
+        private static readonly HashSet<string> _lightRingItemIds = new();
         public static bool Rings_Update_Prefix(Ring __instance, string? ___lightSourceId, GameTime time, GameLocation environment, Farmer who)
         {
+            if (___lightSourceId != null)
+                _lightRingItemIds.Add(__instance.ItemId);
+            if (___lightSourceId == null)
+            {
+                if (_instance.glowRings && _lightRingItemIds.Contains(__instance.ItemId))
+                {
+                    __instance.onEquip(who);
+                    return false;
+                }
+                return true;
+            }
             bool ifGlow = _instance.glowRings;
             if (ifGlow)
             {
